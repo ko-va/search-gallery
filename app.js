95ca65f3abdd81c88554ef27a1604fd23e22119d
Valentina Kochegarova <kochegarova_v@yahoo.com>
2021-10-08 13:01:35 +0200

    final commit

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 const searchContainer = document.getElementById('searchContainer');
 const galleryContainer = document.getElementById('galleryContainer');
 const navigationContainer = document.getElementById('navigationContainer');
+const footerContainer = document.getElementById('footerContainer');
 
-const apiKey = 'd32660754fde2d5cc4d58253df6d3cee';
+const apiKey = 'b54580f369a7eeebecb2004dc429d08f';
 
 let state = {
   searchPage: 0,
   searchResults: {},
-  page: 'search',
+  page: 'Search',
   galleryImages: {},
+  searchQuery: '',
 }
 
 function initialize() {
@@ -28,8 +30,7 @@ function persist() {
 initialize()
 
 async function search(page = 1) {
-  const searchQuery = document.getElementById('searchBar').value;
-  const result = await fetch(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&text=${searchQuery}&format=json&nojsoncallback=1&page=${page}`);
+  const result = await fetch(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&text=${state.searchQuery}&format=json&nojsoncallback=1&page=${page}`);
   const data = await result.json();
 
   // if loading first page, unset previous data
@@ -39,18 +40,30 @@ async function search(page = 1) {
 
   const returnedResults = {};
 
-  data.photos.photo.forEach(photo => {
-    returnedResults[photo.id] = photo
-  });
+  if (data.photos && data.photos.photo) {
+    data.photos.photo.forEach(photo => {
+      returnedResults[photo.id] = photo
+    });
 
-  // merge previous result with returned results
-  state.searchResults = {...state.searchResults, ...returnedResults};
+    // merge previous result with returned results
+    state.searchResults = {...state.searchResults, ...returnedResults};
 
-  // update page
-  state.searchPage = page;
+    // update page
+    state.searchPage = page;
+  } else if (data.stat === 'fail') {
+    alert(data.message);
+  }
 
   render();
   persist();
+}
+
+function onSearchChanged(e) {
+  if (e.keyCode === 13) {
+    search();
+  }
+
+  state.searchQuery = document.getElementById('searchBar').value;
 }
 
 function addToGallery(id) {
@@ -77,39 +90,42 @@ function showPage(page) {
 function render() {
   const { page, searchResults, galleryImages, searchPage } = state
 
+  footerContainer.innerHTML = ''
+
   // if we're on search page, show search input
-  if (page === 'search') {
+  if (page === 'Search') {
     searchContainer.innerHTML = `
-        <input type="text" name="searchBar" id="searchBar" placeholder="search for an image" />
-        <button type="submit" class="btn" onClick="search()">search</button>
+        <input type="text" name="searchBar" id="searchBar" class="searchBar" placeholder="search for an image" onkeyup="return onSearchChanged(event)" value="${state.searchQuery}" />
+        <button type="submit" class="btn btn__search" onClick="search()">search</button>
     `
+
+    if (searchPage > 0) {
+      footerContainer.innerHTML = `<button type="button" class="btn btn--more" onClick="search(${searchPage + 1})">More</button>`
+    }
   } else {
     searchContainer.innerHTML = '';
   }
 
   // toggle page
-  const otherPage = page === 'gallery' ? 'search' : 'gallery'
+  const otherPage = page === 'Gallery' ? 'Search' : 'Gallery'
 
   navigationContainer.innerHTML = `
     <button type="button" class="btn" onClick="showPage('${otherPage}')">Show ${otherPage}</button>
   `
 
   // conditionally select collection based on page (search results or gallery collection)
-  const images = page === 'search' ? Object.values(searchResults) : Object.values(galleryImages)
+  const images = page === 'Search' ? Object.values(searchResults) : Object.values(galleryImages)
 
-  const html = `<div>
+  const html = `<div class="gallery">
     ${images.map(result => {
         const isInGallery = !!galleryImages[result.id]
 
-        return `<div>
-            <img src="https://live.staticflickr.com/${result.server}/${result.id}_${result.secret}.jpg" />
-            <button type="button" class="btn" onClick="${isInGallery ? 'removeFromGallery' : 'addToGallery'}(${result.id})">${isInGallery ? '-' : '+'}</button>
+        return `<div class="imageContainer">
+            <img src="https://live.staticflickr.com/${result.server}/${result.id}_${result.secret}.jpg" class="gallery__img"/>
+            <button type="button" class="btn btn--fav" onClick="${isInGallery ? 'removeFromGallery' : 'addToGallery'}(${result.id})">${isInGallery ? '-' : '+'}</button>
         </div>`
-    })}
-
-    ${page === 'search' && searchPage > 0 ? `<button type="button" class="btn" onClick="search(${searchPage + 1})">load more</button>` : ''}
+    }).join('')}
   </div>`
 
   galleryContainer.innerHTML = html;
 }
-
